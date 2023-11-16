29957f9e7bd4313d3409fe1b47cf46a967881faa
Eugene asaulyak <jenya.asaulyak@gmail.com>
2023-11-16 20:52:38 +0200

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function changeBodyColor(color) {
+  document.querySelector('span.color').textContent = color;
+  document.body.style.backgroundColor = color;
+}
+
+function bindEvents() {
+  document.querySelector('button.change-color')?.addEventListener('click', () => changeBodyColor(getRandomHexColor()));
+}
+
+bindEvents();
